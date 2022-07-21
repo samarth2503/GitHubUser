50ba2db5e0c169711777cf079752f9749a155136
Jain <samarjain@deloitte.com>
2022-07-21 23:28:41 +0530

    added/deleted/updated files

---
+++ src/Pages/SearchPage.java
@@ -0,0 +1,13 @@
+package Pages;
+
+public class SearchPage {
+	
+	public void serach()
+	{
+		System.out.println("Hello LP");
+		String un = "admin";
+		String pwd = "admin";
+		System.out.println("Logged in");
+	}
+
+}
